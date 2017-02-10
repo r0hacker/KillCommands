0c35de1d4ff31d427ebb09bf5e1620b1ce9fc6c2
Tacos <vitalik2000s@gmail.com>
2017-02-10 10:59:37 +0200

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,35 @@
+package tacos.killcomands;
+
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.Plugin;
+import org.bukkit.event.Listener;
+import org.bukkit.event.EventHandler;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.event.entity.PlayerDeathEvent;
+
+public class Main extends JavaPlugin implements Listener {
+
+    @Override
+    public void onEnable() {
+        getLogger().fine("\u00a7cPlugin created by Tacos!");
+        
+        getConfig().options().copyDefaults(true);
+        saveConfig();
+        
+        getServer().getPluginManager().registerEvents((Listener) this, (Plugin) this);
+    }
+
+    @EventHandler
+    public void onPlayerKill(PlayerDeathEvent e) {
+        Player p = e.getEntity();
+        Player killer = p.getKiller();
+
+        for (String s : getConfig().getStringList("commands")) {
+            cmdExec(s.replace("<target>", p.getName()).replace("<killer>", killer.getName()));
+        }
+    }
+
+    private void cmdExec(String s) {
+        getServer().dispatchCommand(getServer().getConsoleSender(), s.replace("&", "\u00a7"));
+    }
+}
